d92880313b639bf65dfde702e7a206f8ddb0aeb1
mido210 <midoriya2109@gmail.com>
2025-04-01 16:04:09 +0900

    main작업완료

---
+++ src/main/java/com/example/demo/smaple1/Sungjuck.java
@@ -0,0 +1,12 @@
+package com.example.demo.smaple1;
+
+import lombok.*;
+import org.springframework.lang.*;
+
+@Getter
+@ToString
+@AllArgsConstructor
+public class Sungjuck {
+    private  String name;
+    private int score;
+}
